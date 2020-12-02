04ab0486c66b89eb2ccd07392435d6328a96600f
cruzlu <cruzlu@careerintlinc.com>
2020-12-02 16:46:20 +0800

    first commmit

---
+++ frontend/models/Test.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $sex
+ * @property integer $tel
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['tel'], 'integer'],
+            [['name', 'sex'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'sex' => 'Sex',
+            'tel' => 'Tel',
+        ];
+    }
+}
